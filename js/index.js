efd6a3bb315d0b0a731577897f348116a455adb2
AgustinPerpen <agustinperpen@hotmail.com>
2021-11-15 20:12:44 -0300

    Logica de js realizado, index ordenado y css bonito

--- js/index.js
+++ js/index.js
@@ -1,43 +1,61 @@
 function validarCantidadKwh(){
-      var cantidad = document.getElementById("cantidad");
-            if(isNaN(cantidad.value == "")){
-                  alert ("El campo cantidad tiene que se numero");
+      let cantidad = document.getElementById("cantidad");
+            if(cantidad.value <= 0){
+                  alert ("El campo cantidad tiene que ser mayor a 0");
                   return false;
             }
 }
 
 function validarDistritos(){
-      var optForm = document.forms["frmCalc"]["distritos"].selectedIndex;
-            if( optForm == null || optForm == 0 ) {
+      let optForm = document.forms["frmCalc"]["distritos"].selectedIndex;
+            if( optForm == undefined || optForm == 0 ) {
 	            alert("Debe seleccionar una opción en el campo distritos");
-	      return false;					
+	      return false;			
       }
 }
 
-function calcular(){
-      var iva = document.getElementById('usuarios').value;
-      var zonas = document.getElementById("distritos").value;
-      var cantidad = document.getElementById("cantidad").value;
-      
-      if (iva == residencial ){
-            iva = 0.21
-      }else if (iva == industrial){
-            iva = 0.27
+/* function validarCheckbox(){
+
+      if (document.querySelector('input[name="usuarios"]:checked').value == undefined){
+            alert("Debe escoger un Tipo de Usuario");
+            return false;
+      }
+} */
+
+function calcularKwh(){
+      let iva = document.querySelector('input[name="usuarios"]:checked').value;
+      let zonas = document.getElementById("distritos").value;
+      let zonaCentro = document.getElementById("distrito-centro").value;
+      let zonaSur = document.getElementById("distrito-sur").value;
+      let zonaOeste = document.getElementById("distrito-oeste").value;
+      let zonaNorte = document.getElementById("distrito-norte").value;
+      let cantidad = document.getElementById("cantidad").value;
+      let ivaFinal;
+      let zonasFinal;
+      let resu = document.getElementById("result");
+
+      if (iva == "residencial"){
+            ivaFinal = 0.21;
+      }
+      if (iva == "industrial"){
+            ivaFinal = 0.27;
       }
       
-      if (zonas == "distrito-centro" ){
-            zonas = 5.80;
-       }
-      if (zonas =="distrito-sur" ){
-            zonas = 5.40;
-       }    
-      if (zonas =="distrito-oeste" ){   
-            zonas = 5.35;
-       }
-       if (zonas =="distrito-norte"){
-            zonas = 5.60;
+      if (zonas == zonaCentro){
+            zonasFinal = 5.80;
+       } 
+      if (zonas == zonaSur){
+            zonasFinal = 5.40;
+       } 
+      if (zonas == zonaOeste){  
+            zonasFinal = 5.35;
+       } 
+      if (zonas == zonaNorte){
+            zonasFinal = 5.60;
        }       
-         
-      total = 102 + (cantidad * zonas) * (1 + iva)
-    
+
+      resultado = 102 + (cantidad * zonasFinal) * (1 + ivaFinal);
+      resu.innerText = `$${resultado}`;
+     
 }
+
